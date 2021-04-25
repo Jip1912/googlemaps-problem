5f49b2656a508daf9a3c0cbfd6930e69064cfe80
Jip1912 <jip1912@hotmail.com>
2021-04-25 13:49:21 +0200

    reconstruting problem

--- lib/main.dart
+++ lib/main.dart
@@ -1,113 +1,142 @@
+import 'dart:typed_data';
+import 'dart:ui' as ui;
 import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'dart:async';
+
 
 void main() {
-  runApp(MyApp());
+  runApp(HomePage());
 }
 
-class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
+class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
-      ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+        home: Scaffold(
+          body: FireMap()
+        )
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
+class FireMap extends StatefulWidget {
   @override
-  _MyHomePageState createState() => _MyHomePageState();
+  State createState() => FireMapState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+class FireMapState extends State<FireMap> {
+  GoogleMapController mapController;
+  String _mapStyle;
 
-  void _incrementCounter() {
+  BitmapDescriptor customIcon;
+  Set<Marker> _markers = {};
+
+  @override
+  void initState() {
+    super.initState();
+    getData();
+  }
+
+  Future<void> getData() async {
+    var list = new Set<GeoPoint>();
+    GeoPoint testPoint = new GeoPoint(52.07961021059982, 4.313284507953123); //just a constant point for testing purposes
+    list.add(testPoint);
+    BitmapDescriptor testIcon = await getCustomIcon(circleAvatarKey);
+    int id = 0;
     setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
+      for(GeoPoint l in list) {
+          _markers.add(
+          Marker(
+            markerId: MarkerId(id.toString()),
+            position: LatLng(l.latitude, l.longitude),
+            icon: testIcon == null ? BitmapDescriptor.defaultMarker : testIcon
+          )
+        );
+        id++;
+      }
     });
+  }
+  
+  GlobalKey circleAvatarKey = GlobalKey();
+
+  Future<String> _getProfilePic() { //Normally this function get's the profile picture url, but I just made a future which returns a constant image for testing purposes
+    var completer = new Completer<String>();
+    completer.complete("https://logowik.com/content/uploads/images/flutter5786.jpg");
+    return completer.future;
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
+    return Stack(
+      children: [
+        FutureBuilder<String>(
+          future: _getProfilePic(), // getting the profile picture from the cloud storage
+          builder: (BuildContext context, AsyncSnapshot<String> snapshot) {
+            Widget result;
+            if (snapshot.hasData) {
+              result = RepaintBoundary(
+                child: CircleAvatar(
+                  radius: 30,
+                  backgroundImage: NetworkImage(snapshot.data),
+                ),
+                key: circleAvatarKey
+              );
+            } else if (snapshot.hasError) {
+              result = RepaintBoundary(
+                child: Text(snapshot.error)
+              );
+            } else {
+              result = SizedBox(
+                child: CircularProgressIndicator(),
+                width: 60,
+                height: 60,
+              );
+            }
+            return result;
+          },
         ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+        GoogleMap(
+          initialCameraPosition: CameraPosition(target: LatLng(52.07961021059982, 4.313284507953123), zoom: 15),
+            onMapCreated: (GoogleMapController controller) {
+            mapController = controller;
+            mapController.setMapStyle(_mapStyle);
+          },
+          markers: _markers,
+          myLocationEnabled: true,
+          myLocationButtonEnabled: false,
+          zoomControlsEnabled: false,
+          mapToolbarEnabled: false,
+        ),
+      ]
     );
+  }
+
+  Future<BitmapDescriptor> getCustomIcon(GlobalKey iconKey) async {
+    Future<Uint8List> _capturePng(GlobalKey iconKey) async {
+      try {
+        print('inside');
+        RenderRepaintBoundary boundary = iconKey.currentContext.findRenderObject();
+        ui.Image image = await boundary.toImage(pixelRatio: 3.0);
+        ByteData byteData = await image.toByteData(format: ui.ImageByteFormat.png);
+        var pngBytes = byteData.buffer.asUint8List();
+        print(pngBytes);
+        return pngBytes;
+      } catch (e) {
+        print(e);
+      }
+    }
+
+    Uint8List imageData = await _capturePng(iconKey);
+    print("testIcon set");
+    return BitmapDescriptor.fromBytes(imageData);
+  }
+
+  void _onMapCreated(GoogleMapController controller) {
+    setState(() {
+      mapController = controller;
+    });
   }
 }
